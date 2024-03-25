1041f967ef3333c3de2c96e147dd2ee2ca875d60
Ing. Fernando <idsfernandomorales@gmail.com>
2024-03-25 17:09:53 -0600

    backend folder created

---
+++ bitrestaurant-api/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    indent: ["error", 4],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "max-len": [
+      "error",
+      {
+        code: 80,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreComments: true,
+      },
+    ],
+    "brace-style": ["error", "1tbs", { allowSingleLine: true }],
+    "space-before-blocks": "error",
+    "space-before-function-paren": ["error", "always"],
+    "no-unused-vars": "error",
+    "no-multiple-empty-lines": ["error", { max: 2, maxEOF: 1 }],
+    "eol-last": "error",
+  },
+};
